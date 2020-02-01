eeb61cd02ac3575d74e8c3fd43c6bb7833899143
Vivica Ma <vivicacongma@gmail.com>
2020-02-01 17:22:10 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
-# Write your #display_rainbow method here
+def display_rainbow (color)
+  puts "#{color.charAt(0)}"
+  puts "#{color}"
+
+end
+color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+print display_rainbow(color)    # Write your #display_rainbow method here
